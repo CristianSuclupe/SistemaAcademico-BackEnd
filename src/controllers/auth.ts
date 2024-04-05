014b6b629196e6f738571b898b72c54a7359367c
Cristian Omar Suclupe Llontop <N00148478@upn.pe>
2024-04-04 19:55:23 -0500

    prueba

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,26 +1,27 @@
-import { Request, Response } from 'express'
-import { HttpResponse } from '../utils/httpResponse'
-import { findUser, validatePassword } from '../utils/shared'
-import { JwtService } from './../services/jwt'
+import { Request, Response } from "express";
+import { HttpResponse } from "../utils/httpResponse";
+import { findUser, validatePassword } from "../utils/shared";
+import { JwtService } from "./../services/jwt";
 
 export class AuthController {
-  private readonly httpResponse = new HttpResponse()
-  private readonly jwtService = new JwtService()
+  private readonly httpResponse = new HttpResponse();
+  private readonly jwtService = new JwtService();
 
   login = async (req: Request, res: Response) => {
-    const { user, password, rol } = req.body
-    if (!rol) return this.httpResponse.BadRequest(res, 'Invalid user type')
+    const { user, password, rol } = req.body;
+    console.log(user, password, rol);
+    if (!rol) return this.httpResponse.BadRequest(res, "Invalid user type");
     try {
-      const userSearched = await findUser(rol, user)
+      const userSearched = await findUser(rol, user);
       const passwordSearched = userSearched
         ? await validatePassword(password, userSearched.password)
-        : false
+        : false;
       if (!userSearched || !passwordSearched)
-        return this.httpResponse.NotFound(res, 'Incorrect user or password')
-      const token = this.jwtService.createToken(userSearched)
-      return this.httpResponse.Ok(res, token)
+        return this.httpResponse.NotFound(res, "Incorrect user or password");
+      const token = this.jwtService.createToken(userSearched);
+      return this.httpResponse.Ok(res, token);
     } catch (error) {
-      return this.httpResponse.InternalServerError(res, 'No data')
+      return this.httpResponse.InternalServerError(res, "No data");
     }
-  }
+  };
 }
